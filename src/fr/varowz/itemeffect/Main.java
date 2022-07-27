27f50bdcad047440e48a57ad045ba6b79b6714d0
VaroWz <antoine.bouille.24@gmail.com>
2022-07-27 22:36:36 +0200

    Main class creation

---
+++ src/fr/varowz/itemeffect/Main.java
@@ -0,0 +1,41 @@
+package fr.varowz.itemeffect;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	
+	private String prefix;
+	
+	@Override
+	public void onEnable() {
+		
+		saveDefaultConfig();
+		
+		setPrefix(getConfig().getString("Lang.Prefix").replace("&", "§"));
+		
+		Bukkit.getConsoleSender().sendMessage(getPrefix() + "§aPlugin §7[§e1.0.0§7] §ais online.");
+		Bukkit.getConsoleSender().sendMessage(getPrefix() + "§aPlugin §7[§e1.0.0§7] §cDev by VaroWz.");
+		Bukkit.getConsoleSender().sendMessage(getPrefix() + "§aPlugin §7[§e1.0.0§7] §9Discord: https://discord.gg/SbKrKehCpq.");
+		
+	}
+	
+	
+	@Override
+	public void onDisable() {
+		
+		Bukkit.getConsoleSender().sendMessage(getPrefix() + "§cPlugin §7[§e1.0.4§7] §cis offline.");
+		
+	}
+
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+}
